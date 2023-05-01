82cdf29c09b32d40eb5cf713a4c7d176291bf46e
vytruongle <truongvy18121999@gmail.com>
2023-05-02 00:15:44 +0700

    update function

--- src/pages/contents/Personal/Personal.jsx
+++ src/pages/contents/Personal/Personal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 //scss
 import styles from "../../../sass/contents/Personal.module.scss";
@@ -10,29 +10,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { PAUSE_SONG, PLAY_SONG } from "../../../actions/audioSlice";
 import { CHOOSE_SONG } from "../../../actions/listSlice";
 import { useNavigate } from "react-router-dom";
-import {
-  ADD,
-  ADD_PLAYLIST,
-  DEL,
-  DEL_PLAYLIST,
-} from "../../../actions/manageUser";
 
 const Personal = () => {
   const { registerList, indexUser } = useSelector((state) => state.manageUser);
-  const [listSong, setList] = useState([]);
-  const [playlistSong, setPlaylist] = useState([]);
+  const listSong = registerList[indexUser].favorPlaySong || [];
+  const playlistSong = registerList[indexUser].favorPlayList || [];
   const isPlaying = useSelector((state) => state.audioReducer.isPlaySong);
   const index = useSelector((state) => state.listReducer.chooseSong.id);
   const titleSong = useSelector((state) => state.listReducer.chooseSong.title);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (registerList.length > 0) {
-      setList(registerList[indexUser].favorPlaySong);
-      setPlaylist(registerList[indexUser].favorPlayList);
-    }
-  }, [registerList, indexUser, ADD, ADD_PLAYLIST, DEL, DEL_PLAYLIST]);
+  // useEffect(() => {
+  //   if (registerList.length > 0) {
+  //     setList();
+  //     setPlaylist();
+  //   }
+  // }, [registerList, indexUser, ADD, ADD_PLAYLIST, DEL, DEL_PLAYLIST]);
 
   const renderFavorSong = () => {
     return listSong?.map((item, id) => {
